208d383fe12d86ec8536b42cb411bd3ec2b4d81c
Alexander Obregon <alexander.obregon47@gmail.com>
2021-11-28 20:20:31 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solutions in this file
+
+
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+
+  }
+
+  return gifts;
+
+}
+
+wrapGifts(gifts);
+*/
+
+const card = [];
+
+function writeCards(names, event) {
+ for (let i = 0; i < names.length; i ++) {
+   card.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  debugger;}
+   return card;
+  }
+
+function countDown() {
+  let countDown = 10;
+  while (countDown > -1) {
+    console.log(countDown--);
+  }
+  return countDown
+}
+
+
+wrapGifts(gifts);
+
+
+
